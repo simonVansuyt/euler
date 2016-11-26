e774b6dafecf46aa719350e82a059434085addf0
simon vansuyt <simonVansuyt@users.noreply.github.com>
2016-11-26 14:40:30 +0100

    Update project_015.hs

--- project_015.hs
+++ project_015.hs
@@ -5,6 +5,6 @@ import Numeric
 binomial n k = factorial(n) / (factorial(k) * factorial((n-k)))
   where factorial x = if x > 1 then factorial(x-1) * x else 1
 
--- Haskell put's solution in scientific notation
+-- Haskell put solution in scientific notation
 project_15 = putStrLn $ Numeric.showFFloat Nothing solution "" 
   where solution = binomial 40 20
