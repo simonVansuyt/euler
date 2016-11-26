8fb0ac24b20fdf23f47b1bbe73b6c4e4d6ae858c
simonVansuyt <simon.vansuyt@gmail.com>
2016-11-26 14:39:17 +0100

    binomial coefficient

--- project_015.hs
+++ project_015.hs
@@ -1,7 +1,10 @@
+-- the math way for this problem is by calculation the binomial
+-- find more about it here https://en.wikipedia.org/wiki/Lattice_path
 import Numeric
 
 binomial n k = factorial(n) / (factorial(k) * factorial((n-k)))
   where factorial x = if x > 1 then factorial(x-1) * x else 1
 
+-- Haskell put's solution in scientific notation
 project_15 = putStrLn $ Numeric.showFFloat Nothing solution "" 
   where solution = binomial 40 20
